e5fbb620a52b4da175a4a5f9526d6c136ba4d51e
chushul06 <tgarvit260@gmail.com>
2026-05-25 00:35:20 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory.jsx'
 import Pricing from './pages/Pricing.jsx'
 import InterviewReport from './pages/InterviewReport.jsx'
 
-export const ServerUrl = "http://localhost:8000"
+export const ServerUrl = "https://interviewiq-nu9u.onrender.com"
 const App = () => {
   const dispatch = useDispatch()
   useEffect(() => {
